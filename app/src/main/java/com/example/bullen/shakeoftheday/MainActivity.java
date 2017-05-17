f6214aba67255c9f1a0608dbdbb333999a501f8d
bullen <oscar_andersson94@hotmail.com>
2017-05-17 11:13:19 +0200

    välkommen

---
+++ app/src/main/java/com/example/bullen/shakeoftheday/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.bullen.shakeoftheday;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = "DagensShakeApp";
+
+
+        public void init(){
+        Button knapp = (Button) findViewById(R.id.knapp);
+        knapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent shake = new Intent(MainActivity.this, Shaket.class);
+                startActivity(shake);
+                Log.i(TAG, "Det här är dagens shake!");
+                Toast.makeText(getApplicationContext(), "Här har du ditt shake!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+}}
---
+++ app/src/main/java/com/example/bullen/shakeoftheday/Shaket.java
@@ -0,0 +1,13 @@
+package com.example.bullen.shakeoftheday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shaket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shaket);
+    }
+}
