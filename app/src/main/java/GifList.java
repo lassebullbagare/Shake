0345cffcddf8cc7838da06d029762c371ab32159
bullen <oscar_andersson94@hotmail.com>
2017-05-17 11:12:26 +0200

    välkommen

---
+++ app/src/main/java/GifList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * Created by bullen on 2017-05-11.
+ */
+
+public class GifList {
+    ArrayList<String> gif = new ArrayList<>();
+    gif.add(0, "drawable/skak.gif");
+    gif.add(1, "drawable/high.gif");
+    gif.add(2, "drawable/will.gif");
+    gif.add(3, "drawable/cool.gif");
+    gif.add(4, "drawable/kram.gif");
+    gif.add(5, "drawable/kram2.gif");
+    gif.add(6, "drawable/chest.gif");
+}
